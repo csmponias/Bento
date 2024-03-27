d6b0d80e12d0346386c50d07928b8e75406d3228
csmponias <162926803+csmponias@users.noreply.github.com>
2024-03-27 13:03:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,16 +11,16 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'Charalampe',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
 	// Greetings
-	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,',
-	greetingEvening: 'Good evening,',
-	greetingNight: 'Go to Sleep!',
+	greetingMorning: 'Kalimera,',
+	greetingAfternoon: 'Kalispera,',
+	greetingEvening: 'Kalo apogevma,',
+	greetingNight: 'Kalinixta,',
 
 	// Layout
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
